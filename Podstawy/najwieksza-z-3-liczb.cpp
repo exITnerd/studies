1f577c7e4c5dc2b751fae87c598d5f308b8c1023
Wojciech Głogowski <91673707+exITnerd@users.noreply.github.com>
2022-12-14 20:51:11 +0100

    Update najwieksza-z-3-liczb.cpp

--- Podstawy/najwieksza-z-3-liczb.cpp
+++ Podstawy/najwieksza-z-3-liczb.cpp
@@ -1,4 +1,6 @@
 //Napisz program, który wczytuje ze standardowego wejścia trzy liczby całkowite i wypisuje na standardowym wyjściu największą z ich wartości (pamiętaj o przypadku gdy wszystkie podane liczby lub dwie z nich są równe).  
+#include <iostream>
+
 int main()
 {
     int c;
