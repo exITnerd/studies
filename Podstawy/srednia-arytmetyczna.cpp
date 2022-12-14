9d978a44cb834c49ad5ca54a0e6cfe527669ad56
Wojciech Głogowski <91673707+exITnerd@users.noreply.github.com>
2022-12-14 20:43:45 +0100

    Update srednia-arytmetyczna.cpp

--- Podstawy/srednia-arytmetyczna.cpp
+++ Podstawy/srednia-arytmetyczna.cpp
@@ -1,11 +1,17 @@
 //Napisz  program,  który  wczytuje  ze  standardowego  wejścia  trzy  liczby  całkowite  i  wypisuje  na standardowym wyjściu ich średnią arytmetyczną.
-int a;
-int b;
-int c;
-float srednia;
-printf("Podaj trzy liczby całkowite, aby obliczyć ich średnią\n");
-scanf_s("%i", &a);
-scanf_s("%i", &b);
-scanf_s("%i", &c);
-srednia = (float)(a + b + c) / 3;
-printf("%f\n", srednia);
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+  int a;
+  int b;
+  int c;
+  float srednia;
+  printf("Podaj trzy liczby całkowite, aby obliczyć ich średnią\n");
+  scanf_s("%i", &a);
+  scanf_s("%i", &b);
+  scanf_s("%i", &c);
+  srednia = (float)(a + b + c) / 3;
+  printf("%f\n", srednia);
+}
