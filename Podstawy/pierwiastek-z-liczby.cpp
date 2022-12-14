33a24a68b0d3e31e1f7b2a3e9c677be2c2309560
Wojciech Głogowski <91673707+exITnerd@users.noreply.github.com>
2022-12-14 20:40:42 +0100

    Update pierwiastek-z-liczby.cpp

--- Podstawy/pierwiastek-z-liczby.cpp
+++ Podstawy/pierwiastek-z-liczby.cpp
@@ -1,14 +1,20 @@
 //Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę wymierną x i wypisuje na standardowym wyjściu √x. 
-float x;
-double pierwiastek;
-printf("Podaj liczbę wymierną, aby obliczyć z niej pierwiastek\n");
-scanf_s("%f", &x);
-    if (x >= 0) 
-    {
-        pierwiastek = sqrt(x);
-        printf("Pierwiastek podanej liczby to: %f\n", pierwiastek);
-    }
-    else
-    {
-        printf("Liczba jest ujemna, nie można znaleźć pierwiastka!\n");
-    }
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    float x;
+    double pierwiastek;
+    printf("Podaj liczbę wymierną, aby obliczyć z niej pierwiastek\n");
+    scanf_s("%f", &x);
+        if (x >= 0) 
+        {
+            pierwiastek = sqrt(x);
+            printf("Pierwiastek podanej liczby to: %f\n", pierwiastek);
+        }
+        else
+        {
+            printf("Liczba jest ujemna, nie można znaleźć pierwiastka!\n");
+        }
+}
