d7469885733427979a3a4cdfb797951225ecaa87
Wojciech Głogowski <91673707+exITnerd@users.noreply.github.com>
2022-12-14 20:40:03 +0100

    Update notacja-wykladnicza.cpp

--- Podstawy/notacja-wykladnicza.cpp
+++ Podstawy/notacja-wykladnicza.cpp
@@ -1,5 +1,11 @@
 //Napisz  program,  który  wczytuje  ze  standardowego  wejścia  liczbę  wymierną  i  wypisuje  ją  na standardowym wyjściu w notacji wykładniczej (czyli takiej, w której 0.2 to 2.0e-1).
-float ntwy;
-printf("Podaj dowolną liczbę wymierną\n");
-scanf_s("%f", &ntwy);
-printf("Ta liczba w notacji wykładniczej to: %E\n", ntwy);
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+  float ntwy;
+  printf("Podaj dowolną liczbę wymierną\n");
+  scanf_s("%f", &ntwy);
+  printf("Ta liczba w notacji wykładniczej to: %E\n", ntwy);
+}
